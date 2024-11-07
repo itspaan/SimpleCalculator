99127e69fbbdb91d9817e151ac834daafb654ad6
itspaan <ffadilahh0@gmail.com>
2024-11-07 22:06:03 +0700

    Delete SimpleCalculatorPaan directory

--- SimpleCalculatorPaan/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Calculator","l":"actionPerformed(ActionEvent)","u":"actionPerformed(java.awt.event.ActionEvent)"},{"p":"<Unnamed>","c":"Calculator","l":"Calculator()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Calculator","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
